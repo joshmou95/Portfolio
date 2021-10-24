599471732e4e7797151ed5b38e90b1823db9c8b0
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-10-24 14:44:59 -0600

    update styles, add animations

--- js/script.js
+++ js/script.js
@@ -1,58 +1,64 @@
-var fields = {};
+// (function($){
+//   new WOW().init();
+// })(jQuery);
 
-document.addEventListener("DOMContentLoaded", function() {
-  fields.yourName = document.getElementById('yourName');
-  fields.email = document.getElementById('email');
-  fields.message = document.getElementById('message');
-})
+// var fields = {};
 
-function isNotEmpty(value) {
-  if (value == null || typeof value == 'undefined' ) return false;
-  return (value.length > 0);
-}
+// document.addEventListener("DOMContentLoaded", function() {
+//   fields.yourName = document.getElementById('yourName');
+//   fields.email = document.getElementById('email');
+//   fields.message = document.getElementById('message');
+// })
 
-function isEmail(email) {
-  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-  return regex.test(String(email).toLowerCase());
-}
+// function isNotEmpty(value) {
+//   if (value == null || typeof value == 'undefined' ) return false;
+//   return (value.length > 0);
+// }
 
-function fieldValidation(field, validationFunction) {
-  if (field == null) return false;
+// function isEmail(email) {
+//   let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+//   return regex.test(String(email).toLowerCase());
+// }
+
+// function fieldValidation(field, validationFunction) {
+//   if (field == null) return false;
  
-  let isFieldValid = validationFunction(field.value)
-  if (!isFieldValid) {
-  field.className = 'placeholderRed';
-  } else {
-  field.className = '';
-  }
+//   let isFieldValid = validationFunction(field.value)
+//   if (!isFieldValid) {
+//   field.className = 'placeholderRed';
+//   } else {
+//   field.className = '';
+//   }
  
-  return isFieldValid;
-}
+//   return isFieldValid;
+// }
 
-function isValid() {
-  var valid = true;
+// function isValid() {
+//   var valid = true;
   
-  valid &= fieldValidation(fields.yourName, isNotEmpty);
-  valid &= fieldValidation(fields.email, isEmail);
-  valid &= fieldValidation(fields.message, isNotEmpty);
+//   valid &= fieldValidation(fields.yourName, isNotEmpty);
+//   valid &= fieldValidation(fields.email, isEmail);
+//   valid &= fieldValidation(fields.message, isNotEmpty);
  
-  return valid;
-}
+//   return valid;
+// }
 
-class User {
-  constructor(yourName, email, message) {
-  this.yourName = yourName;
-  this.email = email;
-  this.message = message;
-  }
-}
+// class User {
+//   constructor(yourName, email, message) {
+//   this.yourName = yourName;
+//   this.email = email;
+//   this.message = message;
+//   }
+// }
 
-function sendContact() {
-  if (isValid()) {
-    let usr = new User(yourName.value, email.value, message.value)
-    alert(`${usr.yourName} thanks for your message!`)
-  } else {
-    alert("Something went wrong...")
-  }
-}
+// function sendContact() {
+//   if (isValid()) {
+//     let usr = new User(yourName.value, email.value, message.value)
+//     alert(`${usr.yourName} thanks for your message!`)
+//   } else {
+//     alert("Something went wrong...")
+//   }
+// }
+
+
 
